9a8886e67ec517b6ac4810fcfdd734a59bd6f598
Lucasarkh <lucasarchanjo1010@gmail.com>
2023-02-25 11:35:19 -0300

    identation details

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,32 +1,27 @@
-
 function getValueComp() {
-    var selectedValueComp = parseFloat(document.getElementById('comp').value);
-    //console.log(selectedValueComp);
-    return(selectedValueComp);
-
+  var selectedValueComp = parseFloat(document.getElementById("comp").value);
+  //console.log(selectedValueComp);
+  return selectedValueComp;
 }
 
 function getValueInterval() {
-    var selectedValueInterval = parseFloat(document.getElementById('interval').value);
-    //console.log(selectedValueInterval);
-    return(selectedValueInterval);
+  var selectedValueInterval = parseFloat(
+    document.getElementById("interval").value
+  );
+  //console.log(selectedValueInterval);
+  return selectedValueInterval;
 }
 
 function getValueDays() {
-    var selectedValueDays = parseFloat(document.getElementById('days').value);
-    //console.log(selectedValueDays);
-    return(selectedValueDays);
+  var selectedValueDays = parseFloat(document.getElementById("days").value);
+  //console.log(selectedValueDays);
+  return selectedValueDays;
 }
 
 function calculate() {
-   var multiply = parseFloat((getValueComp() * getValueDays()) * getValueInterval());
-   console.log(multiply)
-   document.getElementById('result').innerHTML = multiply 
+  var multiply = parseFloat(
+    getValueComp() * getValueDays() * getValueInterval()
+  );
+  console.log(multiply);
+  document.getElementById("result").innerHTML = multiply;
 }
-
-
-
-
-
-
-
