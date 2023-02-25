e3afb82b59459eb31e5b802a2c8c79d618d00e3b
Lucasarkh <lucasarchanjo1010@gmail.com>
2023-02-24 23:07:06 -0300

    js bugs solved

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,20 +1,31 @@
 
 function getValueComp() {
     var selectedValueComp = parseFloat(document.getElementById('comp').value);
+    //console.log(selectedValueComp);
+    return(selectedValueComp);
+
 }
 
 function getValueInterval() {
     var selectedValueInterval = parseFloat(document.getElementById('interval').value);
+    //console.log(selectedValueInterval);
+    return(selectedValueInterval);
 }
 
 function getValueDays() {
-    var selectedValueDays = document.getElementById('days').value;
+    var selectedValueDays = parseFloat(document.getElementById('days').value);
+    //console.log(selectedValueDays);
+    return(selectedValueDays);
 }
 
-const resulting = getValueComp.selectedValueComp * getValueInterval.selectedValueInterval;
+function calculate() {
+   var multiply = parseFloat(getValueComp() * getValueInterval() * getValueDays());
+   console.log(multiply)
+   document.getElementById('result').innerHTML = multiply 
+}
 
 
-console.log(resulting);
+
 
 
 
