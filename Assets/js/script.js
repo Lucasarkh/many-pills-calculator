1cc00a8fd3d529d3f8aea00cd7bcaee10fb90dfe
Lucasarkh <lucasarchanjo1010@gmail.com>
2023-02-24 21:12:12 -0300

    js alteration for improve the code

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,21 @@
-function calcular() {
-    document.getElementById(comp1) = 0.25;
-    
+
+function getValueComp() {
+    var selectedValueComp = parseFloat(document.getElementById('comp').value);
 }
+
+function getValueInterval() {
+    var selectedValueInterval = parseFloat(document.getElementById('interval').value);
+}
+
+function getValueDays() {
+    var selectedValueDays = document.getElementById('days').value;
+}
+
+const resulting = getValueComp.selectedValueComp * getValueInterval.selectedValueInterval;
+
+
+console.log(resulting);
+
+
+
+
